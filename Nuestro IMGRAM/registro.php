30083fd511f9677d9a5bbd172e69be79f0948793
MiguelAngelRequenaRuiz <miguelangelrequenaruiz@gmail.com>
2020-02-11 11:12:02 +0100

    Revisar registro

--- Nuestro IMGRAM/registro.php
+++ Nuestro IMGRAM/registro.php
@@ -15,19 +15,21 @@
     <div class="header">
       <img src="imagenes/logo.png" />
     </div>
+    <form action="" method="post">
     <div class="pizq">
       <input type="text" placeholder="Correo electrónico" class="input" />
       <div class="texto">
-        <input type="password" placeholder="Nombre completo" class="input" />
+        <input type="text" placeholder="Nombre completo" class="input" />
       </div>
       <div class="texto">
-        <input type="password" placeholder="Usuario" class="input" />
+        <input type="text" placeholder="Usuario" class="input" />
       </div>
       <div class="texto">
         <input type="password" placeholder="Contraseña" class="input" />
       </div>
       <input type="button" value="Registrarte" class="boton" />
     </div>
+    </form>
   </div>
   <div class="contenidobajo">
     <div class="partebaja">
