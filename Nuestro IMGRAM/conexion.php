06bc050babf5bb9f86b820a3b509f47c3576d1ce
christiansgesp <christiansg666@gmail.com>
2020-02-11 17:22:43 +0100

    comentario en conex

--- Nuestro IMGRAM/conexion.php
+++ Nuestro IMGRAM/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "photogram");
+$mysqli = new mysqli("localhost", "root", "", "photogram"); //la bd debe llamarse REQUENASOSA y cambiar PHOTOGRAM por imgram
 
 if($mysqli->connect_errno) {
 	echo "Falló la conexión con la base de datos, disculpe las molestias.";
