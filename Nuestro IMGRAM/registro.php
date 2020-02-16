c30643f09b42d4c95970bee8387090d0baa9ae5c
christiansgesp <christiansg666@gmail.com>
2020-02-16 13:48:32 +0100

    save

--- Nuestro IMGRAM/registro.php
+++ Nuestro IMGRAM/registro.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $db_host="localhost";
 $db_user="nombre_de_usuario";
@@ -60,58 +59,3 @@ mysql_close($db_connection);
     mysql_close($db_connection);
 
 ?>
-=======
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <title>IMGram</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
- 
-<body>
-<!-- MEJOR CREAR NUEVOS ID Y CLASS PARA CSS EN ESTA ZONA,
-PORQUE AL HABER EDITADO LAS IMGS EN INDEX ESTO SE HA JODIDO
-UN POCO XD
-TENGO QUE HACER COSAS DE FRANCÉS YA SI LO VES Y TE HACE
-LO TOCAS TÚ -->
-<div id="principal">
-  
-  <div class="contenido">
-    <div class="header">
-      <img src="imagenes/imgramlogo.png" />
-    </div>
-    <form action="" method="post">
-    <div class="pizq">
-      <input type="text" placeholder="Correo electrónico" class="input" />
-      <div class="texto">
-        <input type="text" placeholder="Nombre completo" class="input" />
-      </div>
-      <div class="texto">
-        <input type="text" placeholder="Usuario" class="input" />
-      </div>
-      <div class="texto">
-        <input type="password" placeholder="Contraseña" class="input" />
-      </div>
-      <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Acepto los términos.
-      </label>
-    </div>
-      <input type="button" value="Registrarte" class="boton" />
-    </div>
-    </form>
-  </div>
-  <div class="contenidobajo">
-    <div class="partebaja">
-      ¿Tienes una cuenta? <a href="index.php">Entrar a mi perfil</a>
-    </div>
-  </div>
-
-</div>
-
-</body>
-</html>
->>>>>>> 53920bc310108e845c0854a00c28e533ed5afcb6
