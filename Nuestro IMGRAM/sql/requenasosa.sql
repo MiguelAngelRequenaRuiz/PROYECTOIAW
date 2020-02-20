7bc86f8d3b4a5008ab058f70e9546fc3c1ee0b03
MiguelAngelRequenaRuiz <miguelangelrequenaruiz@gmail.com>
2020-02-20 13:03:02 +0100

    Registro funciona

--- Nuestro IMGRAM/sql/requenasosa.sql
+++ Nuestro IMGRAM/sql/requenasosa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-02-2020 a las 08:41:06
+-- Tiempo de generación: 20-02-2020 a las 13:02:27
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.1
 
@@ -19,8 +19,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `mysql`
+-- Base de datos: `requenasosa`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagenes`
+--
+
+CREATE TABLE `imagenes` (
+  `id` int(4) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `fecha` date NOT NULL,
+  `contrasena` varchar(400) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -32,12 +46,20 @@ CREATE TABLE `usuarios` (
   `id` int(4) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `correo` varchar(50) NOT NULL,
-  `nombre_completo` varchar(50) DEFAULT NULL
+  `nombre_completo` varchar(50) DEFAULT NULL,
+  `contrasena` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -50,10 +72,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
