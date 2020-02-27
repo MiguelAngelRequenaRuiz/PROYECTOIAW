317db158462d74cf1a26cfc99f3885b62cd65c46
christiansgesp <christiansg666@gmail.com>
2020-02-27 14:05:15 +0100

    boton salir

--- IMGRAM/index.php
+++ IMGRAM/index.php
@@ -35,9 +35,10 @@ if(isset($_SESSION['online']) && $_SESSION['online'] == TRUE) {
                   }
               ?>
             <input type="submit" value="Entrar" class="boton-reg" name="entrar" style="margin-top: 10px;margin-bottom: 15px"/>
-              <a href="recuperacion.php"></br>He olvidado mi contraseña</a>
+           
           </form>
           <button type="submit" class="boton-reg" style="margin-top: 15px" OnClick="location.href='home_invitado.php'">Entrar como invitado</button>
+          <a href="recuperacion.php"></br>He olvidado mi contraseña</a>
         </div>
       </div>
 
