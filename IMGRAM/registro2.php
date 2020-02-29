a6557661f8e01bc578d8ea70576f846b7e4efc2b
christiansgesp <christiansg666@gmail.com>
2020-02-29 14:04:51 +0100

    tab

--- IMGRAM/registro2.php
+++ IMGRAM/registro2.php
@@ -12,45 +12,47 @@
 <div id="cont-princ">
   
     <div class="cont-sec">
+
         <div class="cabecera-reg">
-        <img src="imagenes/imgramlogo.png" />
+            <img src="imagenes/imgramlogo.png" />
         </div>
 
         <div class="l-part">
-        <?php
-            $nombre = trim(htmlspecialchars($_REQUEST["usuario"], ENT_QUOTES, "UTF-8"));
-            $correo = trim(htmlspecialchars($_REQUEST["correo"], ENT_QUOTES, "UTF-8"));
-            $nombre_completo = trim(htmlspecialchars($_REQUEST["nombre"], ENT_QUOTES, "UTF-8"));
-            $contrasena_req = trim(htmlspecialchars($_REQUEST["contrasena"], ENT_QUOTES, "UTF-8"));
-            $contrasena_hash = hash('sha256',  $contrasena_req);
+        
+            <?php
+                $nombre = trim(htmlspecialchars($_REQUEST["usuario"], ENT_QUOTES, "UTF-8"));
+                $correo = trim(htmlspecialchars($_REQUEST["correo"], ENT_QUOTES, "UTF-8"));
+                $nombre_completo = trim(htmlspecialchars($_REQUEST["nombre"], ENT_QUOTES, "UTF-8"));
+                $contrasena_req = trim(htmlspecialchars($_REQUEST["contrasena"], ENT_QUOTES, "UTF-8"));
+                $contrasena_hash = hash('sha256',  $contrasena_req);
 
-            $conexion = mysqli_connect("localhost", "admin", "1234", "requenasosa") 
-            or die("Problemas en la conexion");
+                $conexion = mysqli_connect("localhost", "admin", "1234", "requenasosa") 
+                    or die("Problemas en la conexion");
 
 
-            $insercion = "INSERT INTO usuarios (nombre, contrasena, correo, nombre_completo)
+                $insercion = "INSERT INTO usuarios (nombre, contrasena, correo, nombre_completo)
                                 VALUES ('$nombre', '$contrasena_hash', '$correo', '$nombre_completo')";
 
-            if (mysqli_query($conexion, $insercion)) {
-              echo "<p>"."El registro se ha realizado de forma exitosa"."</p>";
-              echo "</br>";
-              $crearcarpeta = "imagenes/".$nombre."/";
-              if (!file_exists($crearcarpeta)) {
-                  mkdir($crearcarpeta, 0777, true);
+                if (mysqli_query($conexion, $insercion)) {
+                    echo "<p>"."El registro se ha realizado de forma exitosa"."</p>";
+                        echo "</br>";
+              
+                $crearcarpeta = "imagenes/".$nombre."/";
+                    if (!file_exists($crearcarpeta)) {
+                        mkdir($crearcarpeta, 0777, true);
               }
-            } else {
-                header('location: registro1.php?error=El usuario ya existe, elija otro por favor');
-            }
-                
-            mysqli_close($conexion);
-        ?>
-        <div class="sobretexto">
-            <button type="submit" class="boton-reg" OnClick="location.href='index.php'">Volver</button>
+                } else {
+                    header('location: registro1.php?error=El usuario ya existe, elija otro por favor');
+                            }
+                    mysqli_close($conexion);
+            ?>
+
+            <div class="sobretexto">
+                <button type="submit" class="boton-reg" OnClick="location.href='index.php'">Volver</button>
+            </div>
         </div>
-            
-        </div>
-        </form>
     </div>
+</div>
 </body>
 </html>
 <?php
