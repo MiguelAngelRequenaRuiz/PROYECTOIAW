2166c6821f62b2a9362883e272e820b7aef7f58a
MiguelAngelRequenaRuiz <miguelangelrequenaruiz@gmail.com>
2020-03-05 10:29:25 +0100

    Terminado

--- IMGRAM/borrar_imagenes.php
+++ IMGRAM/borrar_imagenes.php
@@ -8,14 +8,18 @@
     $conexion = mysqli_connect("localhost", "admin", "1234", "requenasosa")
     or die("Problemas en la conexion");
     $borrar = "DELETE FROM imagenes WHERE id='$id'";
-    $borrado = mysqli_query($conexion, $borrar) or die(mysqli_error($conexion));
     $consulta = "SELECT ubicacion FROM imagenes WHERE id = '$id'";
     $registros = mysqli_query($conexion, $consulta) or die(mysqli_error($conexion));
     $reg = mysqli_fetch_array($registros);
     $ubicacion= $reg['ubicacion'];
+    
     if ($_SESSION['usuario'] == 'administrador') {
+        unlink("$ubicacion");
+        $borrado = mysqli_query($conexion, $borrar) or die(mysqli_error($conexion));
         header('location: home_admin.php');
     } else {
+        unlink("$ubicacion");
+        $borrado = mysqli_query($conexion, $borrar) or die(mysqli_error($conexion));
         header('location: home_usuario.php');
     }
 
--- IMGRAM/borrar_usuario2.php
+++ IMGRAM/borrar_usuario2.php
@@ -10,13 +10,15 @@
     $conexion = mysqli_connect("localhost", "admin", "1234", "requenasosa")
     or die("Problemas en la conexion");
     $borrar_ima = "DELETE FROM imagenes WHERE nombre_usuario='$usuario'";
-    $borrado_ima = mysqli_query($conexion, $borrar_ima) or die(mysqli_error($conexion));
+    
 
     $borrar_usu = "DELETE FROM usuarios WHERE nombre='$usuario'";
+    
+    $carpeta = "imagenes/".$usuario;
+    array_map('unlink', glob("$carpeta/*"));
+        rmdir($carpeta);
+    $borrado_ima = mysqli_query($conexion, $borrar_ima) or die(mysqli_error($conexion));
     $borrado_usu = mysqli_query($conexion, $borrar_usu) or die(mysqli_error($conexion));
     mysqli_close($conexion);
     header('location: home_admin.php');
-
-
-
 ?>
